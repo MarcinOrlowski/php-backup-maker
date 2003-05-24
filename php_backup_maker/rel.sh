51ea2b58f7ca301a54df22c6970be915f22c97c3
borszczuk <carlos@wfmh.org,pl>
2003-05-24 14:43:28 +0000

    - really release 3.3

--- php_backup_maker/rel.sh
+++ php_backup_maker/rel.sh
@@ -26,6 +26,7 @@ tar -zcvf "pdm-${1}.tgz" pdm_${TAG}
 md5sum -b pdm-${1}.tgz >pdm-${1}.md5sum
 echo ${TAG} >pdm_latest_version.txt
 
-lukemftp -u ftp://upload.sf.net/incoming/ pdm-${1}.md5sum pdm-${1}.tgz pdm_latest_version.txt
+lukemftp -u ftp://upload.sf.net/incoming/ pdm-${1}.md5sum pdm-${1}.tgz
+scp pdm-${1}.md5sum pdm-${1}.tgz pdm-latest-version.txt carlos@wfmh.org.pl:files/
 
 cd ${CUR}
